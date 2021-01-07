68ba167174286e96f95e350d1711fb21b9d31351
LordofSea <44285746+LordofSea@users.noreply.github.com>
2021-01-08 00:07:23 +0100

    Update __init__.py

--- rhasspyfuzzywuzzy/__init__.py
+++ rhasspyfuzzywuzzy/__init__.py
@@ -30,34 +30,33 @@ def extract_one_sqlite(query: str, examples_path: str):
         conn.close()
         return result
 
-    c.execute(
-        "SELECT path FROM intents ORDER BY rowid LIMIT 1 OFFSET ?", (result[2],)
-    )
+    c.execute("SELECT path FROM intents ORDER BY rowid LIMIT 1 OFFSET ?", (result[2],))
     best_path = c.fetchone()[0]
+    c.execute("SELECT name FROM intents ORDER BY rowid LIMIT 1 OFFSET ?", (result[2],))
+    name_of_intent = c.fetchone()[0]
 
     conn.close()
 
-    return (result[0][0], best_path, result[1])
+    return (result[0][0], best_path, result[1], name_of_intent)
 
 
 def recognize(
     input_text: str,
     intent_graph: nx.DiGraph,
     examples_path: str,
+    confidence: typing.Optional[typing.Callable[[int], int]] = 70,
     intent_filter: typing.Optional[typing.Callable[[str], bool]] = None,
     extra_converters: typing.Optional[
         typing.Dict[str, typing.Callable[..., typing.Any]]
     ] = None,
 ) -> typing.List[Recognition]:
-    """Find the closest matching intent(s)."""
+    """Find the closest matching intent(s). Default 	confidence 70				"""
     start_time = time.perf_counter()
     intent_filter = intent_filter or (lambda i: True)
 
     # Find closest match
     # pylint: disable=unpacking-non-sequence
-    best_text, best_path, best_score = extract_one_sqlite(
-        fuzz_utils.default_process(input_text), examples_path
-    )
+    best_text, best_path, best_score, name_of_intent = extract_one_sqlite(fuzz_utils.default_process(input_text), examples_path)
     _LOGGER.debug("input=%s, match=%s, score=%s", input_text, best_text, best_score)
 
     end_time = time.perf_counter()
@@ -65,10 +64,12 @@ def recognize(
         best_path, intent_graph, extra_converters=extra_converters
     )
 
-    assert recognition and recognition.intent, "Failed to find a match"
-    recognition.intent.confidence = best_score / 100.0
-    recognition.recognize_seconds = end_time - start_time
-    recognition.raw_text = input_text
-    recognition.raw_tokens = input_text.split()
+    if best_score >= confidence:
+	   # assert recognition and recognition.intent, "Failed to find a match"
+	    recognition.intent.name =  name_of_intent
+	    recognition.intent.confidence = best_score / 100.0
+	    recognition.recognize_seconds = end_time - start_time
+	    recognition.raw_text = input_text
+	    recognition.raw_tokens = input_text.split()
+	    return [recognition]
 
-    return [recognition]
